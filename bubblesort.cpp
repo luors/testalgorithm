b103f9f8030182ab4892eda52507d9f8cbd87894
luors <154457848@qq.com>
2020-03-31 20:02:13 +0800

    add bbsort

---
+++ bubblesort.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<stdio.h>
+#include "common.h"
+
+using namespace std;
+
+template<class T>
+void bbsort( T a[], int len)
+{
+	for( int i = 0;i<len-1;i++)
+	{
+		for (int j=i+1;j<len;j++)
+		{
+			if (a[i] > a[j])
+				swap(a[i],a[j]);
+		}
+	}
+	
+}
+
+int main(int argc , char* argv[])
+{
+	int arr[] = {8,1,2,4,7,11,12,22,5,3,55};
+	int size = sizeof(arr)/sizeof(int);
+	printarr(arr,size);
+	std::cout << "Begin" << endl;
+	bbsort(arr,size);
+	printarr(arr,size);
+	return 0;
+}
+
+
+
+
+
+
---
+++ main.cpp
@@ -0,0 +1,19 @@
+
+#include<iostream>
+#include<time.h>
+#include<stdlib.h>
+using namespace std;
+
+
+int main(int argc , char* argv[])
+{	
+	srand((unsigned) time(NULL));
+	std::cout << rand() << endl;
+	return 0;
+}
+
+
+
+
+
+
