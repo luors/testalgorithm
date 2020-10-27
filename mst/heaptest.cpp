64b1c008913e96c4a5d59c2a13e969b963a9f27a
luors <154457848@qq.com>
2020-10-27 20:27:13 +0800

    heap test

---
+++ mst/heaptest.cpp
@@ -0,0 +1,35 @@
+
+
+#include<iostream>
+#include<vector>
+#include<stdlib.h>
+#include<time.h>
+#include<map>
+#include<algorithm>
+#include<stdio.h>
+#include<sstream>
+using namespace std;
+
+
+int main(int argc, char* argv){
+	int num[] = {22,33,45,1,8,9,99,100,55,}
+	int size = sizeof(num)/sizeof(int);
+	make_heap(&num[0],&num[size]);
+	
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
