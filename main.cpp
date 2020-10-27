0c11bb6e2179f0c21bc081493e9d3ccc5a814e70
luors <luors@localhost.localdomain>
2020-10-27 09:59:16 +0800

    fff

--- main.cpp
+++ main.cpp
@@ -2,13 +2,16 @@
 #include<iostream>
 #include<time.h>
 #include<stdlib.h>
+#include<math.h>
 using namespace std;
 
 
 int main(int argc , char* argv[])
 {	
-	srand((unsigned) time(NULL));
-	std::cout << rand() << endl;
+	//srand((unsigned) time(NULL));
+	double i;
+	i = sqrt(2);
+	std::cout << pow(sqrt(2),2) << endl;
 	return 0;
 }
 
